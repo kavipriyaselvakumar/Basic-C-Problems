bf70a6f7fd6c572c02a3838efd236f59bc4ae910
kavipriyaselvakumar <59027836+kavipriyaselvakumar@users.noreply.github.com>
2021-03-12 11:38:53 +0530

    Add files via upload

---
+++ mergesortedarray.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+	int n1,n2,a[100],b[100],i,j,temp;
+	printf("Enter the size of the two array:");
+	scanf("%d %d",&n1,&n2);
+	printf("Enter the first array element:");
+	for(i=0;i<n1;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Enter the second array element:");
+	for(i=0;i<n2;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	for(i=0;i<n2;i++)
+	{
+		a[n1]=b[i];
+		n1++;
+	}
+	for(i=0;i<n1;i++)
+	{
+		for(j=i;j<n1;j++)
+		{
+			if(a[i]<a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n1;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
